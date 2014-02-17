1087a5537bbbf8457480f60fa9f2895b803572f5
Paul Irish <paul.irish@gmail.com>
2014-02-17 14:35:16 -0800

    popup shouldn't reference ADB anymore.

--- Chrome/chromepackage/background.js
+++ Chrome/chromepackage/background.js
@@ -2,54 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var plugin = document.getElementById('adb-plugin');
-
 chrome.browserAction.setBadgeBackgroundColor({color: '#070'});
 
-function update(oneTime) {
-  var devicesMessage = plugin.devices();
-  var serverIsRunning = devicesMessage.substr(0, 4) === "OKAY";
-  if (serverIsRunning) {
-    var devices = devicesMessage.substr(8).split("\n");
-    chrome.browserAction.setIcon({
-      path: {
-        19: 'on_19.png',
-        38: 'on_38.png'
-      }
-    });
-
-    var deviceCount = devices.length - 1;
-    if (deviceCount) {
-      chrome.browserAction.setBadgeText({text: String(deviceCount)});
-    } else {
-      chrome.browserAction.setBadgeText({text: ''});
-    }
-  } else {
-    chrome.browserAction.setIcon({
-      path: {
-        19: 'off_19.png',
-        38: 'off_38.png'
-      }
-    });
-    chrome.browserAction.setBadgeText({text: ''});
+chrome.browserAction.setIcon({
+  path: {
+    19: 'off_19.png',
+    38: 'off_38.png'
   }
-  if (!oneTime)
-    setTimeout(update, 3000);
-}
-
-update();
-
-function start() {
-  setTimeout(function() { plugin.startServer(); });
-}
-
-function stop() {
-  setTimeout(function() { plugin.killServer(); });
-}
-
-function isServerRunning() {
-  return plugin.devices().substr(0, 4) === "OKAY";
-}
+});
 
 function devices() {
   chrome.tabs.create({url:'chrome://inspect'});
--- Chrome/chromepackage/popup.js
+++ Chrome/chromepackage/popup.js
@@ -1,40 +1,17 @@
-(function () {
-  if (navigator.userAgent.match(/Windows NT/))
-    document.body.classList.add('platform-windows');
-  else if (navigator.userAgent.match(/Mac OS X/))
-    document.body.classList.add('platform-mac');
-  else if (navigator.userAgent.match(/Linux/))
-    document.body.classList.add('platform-linux');
-})();
+
+if (navigator.userAgent.match(/Windows NT/))
+  document.body.classList.add('platform-windows');
+else if (navigator.userAgent.match(/Mac OS X/))
+  document.body.classList.add('platform-mac');
+else if (navigator.userAgent.match(/Linux/))
+  document.body.classList.add('platform-linux');
+
 
 var devices = document.getElementById('devices');
-var start = document.getElementById('start');
-var stop = document.getElementById('stop');
-
-if (chrome.extension.getBackgroundPage().isServerRunning()) {
-  start.classList.add('disabled');
-} else {
-  devices.classList.add('disabled');
-  stop.classList.add('disabled');
-}
 
 devices.addEventListener('click', function () {
-  if (devices.classList.contains('disabled'))
-    return;
+
   chrome.extension.getBackgroundPage().devices();
   window.close();
-}, false);
 
-start.addEventListener('click', function () {
-  if (start.classList.contains('disabled'))
-    return;
-  chrome.extension.getBackgroundPage().start();
-  window.close();
-}, false);
-
-stop.addEventListener('click', function () {
-  if (stop.classList.contains('disabled'))
-    return;
-  chrome.extension.getBackgroundPage().stop();
-  window.close();
 }, false);
